5707f56b878fffa526e3a4f19d52385ff8ece86c
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-06-29 19:12:23 +0600

    first commit

---
+++ src/pages/Bookmarks.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Bookmarks = () => {
+  return (
+    <div>
+      <h2>this bookmarks are coming soon.</h2>
+    </div>
+  );
+};
+
+export default Bookmarks;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+const Home = () => {
+  return (
+    <div className="h-[80px]">
+      <Nav></Nav>
+    </div>
+  );
+};
+
+export default Home;
